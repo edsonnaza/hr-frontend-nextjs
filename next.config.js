3b08f12d50dca0c22d3b81bda310bb9f0297cd6e
edsonnaza <edsonnaza@gmail.com>
2024-04-24 08:40:41 -0400

    add: Fix next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { assetPrefix: process.env.NODE_ENV === "production" ? "/about/" : undefined,};
+const nextConfig = { };//assetPrefix: process.env.NODE_ENV === "production" ? "/about/" : undefined,};
 
 module.exports = nextConfig;
