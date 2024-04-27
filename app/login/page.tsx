e235bed48eb4328d4c198cfc12afff87ad848589
edsonnaza <edsonnaza@gmail.com>
2024-04-27 11:58:04 -0400

    add:Fix responsive acme-logo login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
     <main className="flex items-center justify-center  md:h-screen">
       <div className="relative lg:ring-1 rounded-sm mx-autosm:mt-5 flex w-full max-w-[400px]  flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex w-full h-32 items-end rounded-lg bg-blue-500 justify-center  md:h-36">
-          <div className="w-32 sm:h-32 flex-col   text-white md:w-36 md:mr-12 ">
+          <div className="w-32 mr-4 ml-1 flex-col pb-8  items-center justify-center text-white md:w-36 md:mr-14 ">
             <AcmeLogo />
           </div>
         </div>
