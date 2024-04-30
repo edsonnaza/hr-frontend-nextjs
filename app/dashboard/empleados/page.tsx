a1656ac72cf2fc5752def1fc82e6379b814eeaed
edsonnaza <edsonnaza@gmail.com>
2024-04-29 23:30:25 -0400

    add: empleados folder

--- app/dashboard/empleados/page.tsx
+++ app/dashboard/empleados/page.tsx
@@ -1,4 +1,44 @@
+import Pagination from '@/app/ui/empleados/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/empleados/table';
+import { CreateEmpleado } from '@/app/ui/empleados/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchEmpleadosPages } from '@/app/lib/dataEmpleados';
+ 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Empleados',
+};
 
-export default function Customers() {
-    return <p>Empleados...</p>;
-  }
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchEmpleadosPages(query);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Empleados</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Buscar empleado..." />
+        <CreateEmpleado />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+      <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
